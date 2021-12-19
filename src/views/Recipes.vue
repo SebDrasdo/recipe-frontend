1b7aa19f900fd118b5a201791893096b1610f4c4
SebDrasdo <sepp.sama4gameplay@gmail.com>
2021-12-19 10:04:47 +0100

    .env added

--- src/views/Recipes.vue
+++ src/views/Recipes.vue
@@ -2,13 +2,13 @@
   <h1>Welcome to Recipes</h1>
   <div class="container-fluid">
     <div class="row row-cols-1 row-cols-md-2 g-4">
-      <div class="col" v-for="recipe in recipes" :key="recipe.recipeName">
-        <div class="card">
+      <div class="col" v-for="recipe in recipes" :key="recipe.id">
+        <div class="card h-100">
           <img src="../assets/kaninchenv2.png" class="card-img-top" alt='recipe.recipeName'>
           <div class="card-body">
-            <h5 class="card-title">{{ recipe.Name }}</h5>
+            <h5 class="card-title">{{ recipe.recipeName }}</h5>
             <p class="card-text">
-              Anleitung: {{recipe.description}}
+              {{ recipe.description }}
             </p>
           </div>
         </div>
@@ -22,19 +22,23 @@ export default {
   name: 'Recipes',
   data () {
     return {
-      recipes: [
-        {
-          id: 1,
-          recipeName: 'Quatsch mit Sauce',
-          description: 'alles nicht so schwer',
-          vegan: true
-        }
-      ]
+      recipes: []
     }
+  },
+  mounted () {
+    const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/recipes'
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }
+    fetch(endpoint, requestOptions)
+      .then(response => response.json())
+      .then(result => result.forEach(recipe => {
+        this.recipes.push(recipe)
+      }))
+      .catch(error => console.log('error', error))
   }
 }
 </script>
-
 <style scoped>
-
 </style>
